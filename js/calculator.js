dfa925f32b6d52cfcc153e03cb8f6498834a831f
evgenyrinkevich <rinkevich2000@gmail.com>
2023-05-21 17:39:43 +0300

    Handling Symbols & Numbers

--- js/calculator.js
+++ js/calculator.js
@@ -4,8 +4,23 @@ let previousOperator;
 
 const screen = document.querySelector('.screen');
 
-const buttonClick = (value) => {};
+const buttonClick = (value) => {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+};
 
+const handleSymbol = (symbol) => {};
+
+const handleNumber = (numberString) => {
+  if (buffer === '0') {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+};
 const init = () => {
   document.querySelector('.calc-buttons').addEventListener('click', (e) => {
     buttonClick(e.target.innerText);
